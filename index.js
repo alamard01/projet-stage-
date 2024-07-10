f027d366c2fae6af7cfc49d90caacb65d7ab18e7
alamard01 <arthur.lamard@insa-strasbourg.fr>
2024-07-10 10:59:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import * as WebIFC from "web-ifc";
 import * as fs from "node:fs";
-const filename = "a filename here";
+const filename = "tested_sample_project.ifc";
 const ifcAPI = new WebIFC.IfcAPI();
 await ifcAPI.Init();
 const ifcData = fs.readFileSync(filename);
 let modelID = ifcAPI.OpenModel(ifcData);
 
-let allLines = WebIFC.GetAllLines(modelID);
+let allLines = ifcAPI.GetAllLines(modelID);
 console.log("All Lines:", allLines);
 
-let someLineID = allLines[1]; 
-let line = WebIFC.GetLine(modelID, someLineID);
+let someLineID = allLines.get(1); 
+let line = ifcAPI.GetLine(modelID, someLineID);
 console.log("Line:", line);
 console.log("hello")
  
