d457545e7559f699cf97c5df5b0e51b1c97ef303
alamard01 <arthur.lamard@insa-strasbourg.fr>
2024-07-11 15:17:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import * as WebIFC from "web-ifc";
-import * as fs from "node:fs";
-const filename = "tested_sample_project.ifc";
-const ifcAPI = new WebIFC.IfcAPI();
-await ifcAPI.Init();
-const ifcData = fs.readFileSync(filename);
-let modelID = ifcAPI.OpenModel(ifcData);
+import * as WebIFC from "web-ifc";  //  Importe le module web-ifc (une bibliothèque)
+import * as fs from "node:fs";  // importe le module fs (permet de travailler avec les fichiers)
+const filename = "tested_sample_project.ifc";   //définit la constante filename 
+const ifcAPI = new WebIFC.IfcAPI(); //  crée une nouvelle instance de l'API IFC
+await ifcAPI.Init();    // initialise l'API IFC
+const ifcData = fs.readFileSync(filename); //  Lit le fichier IFC et stocke son contenu dans ifcData
+let modelID = ifcAPI.OpenModel(ifcData);// ouvre le modèle IFC et obtient un ID de modèle
+let allLines = ifcAPI.GetAllLines(modelID);    // Récupère toutes les lignes du modèle
+console.log("All Lines:", allLines);    // Affiche toutes les lignes dans la console
+let someLineID = allLines.get(1); // obtient l'ID de la première ligne
+let line = ifcAPI.GetLine(modelID, someLineID);     // récupère les données de la ligne avec cet ID
+console.log("Line:", line);// affiche les données de la ligne dans la console
 
-let allLines = ifcAPI.GetAllLines(modelID);
-console.log("All Lines:", allLines);
-
-let someLineID = allLines.get(1); 
-let line = ifcAPI.GetLine(modelID, someLineID);
-console.log("Line:", line);
 //console.log("hello")
- 
- 
+
+
