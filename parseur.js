09a34126cdcac4240201962e1a94506bad8db0d6
alamard01 <arthur.lamard@insa-strasbourg.fr>
2024-07-12 16:08:35 +0200

    Update parseur.js

--- parseur.js
+++ parseur.js
@@ -1,10 +1,10 @@
 import fs from 'fs';     // permet la manipulation des fichiers 
 import { parseString } from 'xml2js'; //  permet le parsing 
-const filename = 'IDS_aachen_example.ids';//Chemin vers votre fichier XML
+const filename = 'IDS_aachen_example.ids' ;  //Chemin vers votre fichier XML
 function parseXMLFile(filename) {//  Fonction pour lire et parser un fichier XML
     fs.readFile(filename, 'utf-8', (err, data) => {  // Lit le fichier 
         if (err) {     // vérifie s'il y a une erreur (lecture)
-            console.error(`Erreur lors de la lecture du fichier ${filename}`, err);  //affiche l'erreur 
+        console.error(`Erreur lors de la lecture du fichier ${filename}`, err);  //affiche l'erreur 
             return;         
         }
         parseString(data, (err, result) => {   // Parse le fichier
@@ -12,7 +12,7 @@ function parseXMLFile(filename) {//  Fonction pour lire et parser un fichier XML
                 console.error(`Erreur lors du parsing du fichier ${filename}`, err);  //  Affiche l'erreur 
                 return;                              
             }
-            console.log(`Contenu du fichier ${filename} (parsed) :`, result);  // Affiche le résultat dans la console du parsing 
+        console.log(`Contenu du fichier ${filename} (parsed) :`, result);  // Affiche le résultat dans la console du parsing 
         });
     });
 }
