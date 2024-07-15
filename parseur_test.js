7491a5c6b4705327c565b5d55fc50bae4034c1b6
alamard01 <arthur.lamard@insa-strasbourg.fr>
2024-07-15 16:30:04 +0200

    Update parseur_test.js

--- parseur_test.js
+++ parseur_test.js
@@ -1,17 +1,16 @@
 // essayons d'itérer le parseur 
 
 
-import fs from 'fs';    //permet la manipulation des fichiers 
-const testFolder = 'C:\\Users\\Arthur\\Documents\\projet stage\\projet stage';
+import fs from 'fs';   // permet la manipulation des fichiers 
+const testFolder = 'C:\\Users\\Arthur\\Documents\\projet stage';
 fs.readdir(testFolder, (err, files) => {
-  if (err) { //  Détecte les erreurs de lecture du répertoire 
+  if (err) {//Détecte les erreurs de lecture du répertoire 
     console.error('Erreur lors de la lecture du répertoire', err);
     return;
   }
-  const filteredFiles = files.filter(file => file.includes('ids')); // filtre les fichiers contenant 'ids' 
-  console.log('Fichiers ids trouvés :');   // affiche les fichiers filtrés dans la console 
-
-  filteredFiles.forEach(file => {   // Parcourt les fichiers et les affiche dans la console 
+  const filteredFiles = files.filter(file => file.includes('ids'));   // filtre les fichiers contenant 'ids' 
+  console.log('Fichiers ids trouvés :');//  Affiche les fichiers filtrés dans la console 
+  filteredFiles.forEach(file => { //Parcourt les fichiers et les affiche dans la console 
     console.log(file);
   });
 });
