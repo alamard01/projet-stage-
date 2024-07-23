6795fe2980ec987414065e44d76c5ad86932ff99
alamard01 <arthur.lamard@insa-strasbourg.fr>
2024-07-23 15:28:15 +0200

    Create iteration.js

---
+++ iteration.js
@@ -0,0 +1,13 @@
+import fs from 'fs';   // permet la manipulation des fichiers 
+const testFolder = 'C:\\Users\\Arthur\\Documents\\stage université cardiff\\projet stage';
+fs.readdir(testFolder, (err, files) => {
+  if (err) {//Détecte les erreurs de lecture du répertoire 
+    console.error('Erreur lors de la lecture du répertoire', err);
+    return;
+  }
+  const filteredFiles = files.filter(file => file.includes('ids'));   // filtre les fichiers contenant 'ids' 
+  console.log('Fichiers ids trouvés :');//  Affiche les fichiers filtrés dans la console 
+  filteredFiles.forEach(file => { //Parcourt les fichiers et les affiche dans la console 
+    console.log(file);
+  });
+});
